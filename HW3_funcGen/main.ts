ea162ae1a0e188cf34e3e056c4b5c99cfb407ce8
HelenRud <47782904+HelenRud@users.noreply.github.com>
2019-04-24 16:24:09 +0300

    Generator function

---
+++ HW3_funcGen/main.ts
@@ -0,0 +1,49 @@
+// function* userInput() {
+//     yield "Apple";
+//     yield "Orange";
+//     yield 15;
+//     yield "Cherry";
+// }
+
+// let iterable1 = userInput();
+
+// for (const item of iterable1) { 
+//     if ((typeof item) == 'number'){
+//     console.log('User input number: ' + item);
+//     break;}
+//     else console.log(item);
+
+// }
+
+class MyStringCollection {
+
+    private values:string[] = [];
+
+    constructor(userInput) {
+        this.values= userInput;
+    }
+
+    *getValues() {
+        for(let i = 0; i < this.values.length; i ++) {
+            yield this.values[i];
+        }
+    }
+}
+let userInput = () => (prompt('Input some strings, (comma separated). If your input is number, you will see Error in console.')).split(', ');
+let inputNumber= userInput.length;
+
+let iterable2 = new MyStringCollection(userInput()).getValues();
+
+function isNumber(n: string) {return !isNaN(parseFloat(n)) && !isNaN(n - 0); } 
+
+for (const item of iterable2) {
+    if (isNumber(item)) {
+        console.log('Error');
+        break;
+    } 
+    else 
+    console.log(item);
+    
+
+}
+
